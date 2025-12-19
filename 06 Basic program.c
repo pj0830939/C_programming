095f225e12247cb9cb67416337e84a6f5d0fefab
Priyanka Ankush Jadhav <pj0830939@gmail.com>
2025-12-19 22:24:53 +0530

    Add files via upload

---
+++ 06 Basic program.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdio.h>
+
+int main()
+{
+  float No = 0.0 , sqr = 0.0;
+  
+  
+  printf("\n Enter a number=");
+  scanf("%f",&No );
+  
+  sqr = No * No;
+  
+  printf("\n square of given number is %2f", sqr);
+  
+  getch();
+  return 0;
+}
+on => %d + %d =%d",No1, No2,sum);
+  
+  getch();
+  return 0;
+}
