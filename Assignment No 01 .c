2e6125ed518f05abc9e571230d83f6376ad7d312
Priyanka Ankush Jadhav <pj0830939@gmail.com>
2025-12-22 21:02:25 +0530

    Assignment No 02

---
+++ Assignment No 01 .c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdio.h>
+
+int main()
+{
+   int num;
+   
+   printf("\n Enter the number");
+   scanf("%d",&num);
+   
+   if(num % 2 = = 0);
+     printf("the number is even");
+   else
+     printf("the number is odd");
+     
+  getch();
+  return 0;
+}
