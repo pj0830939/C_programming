e07dbac37663dbd89ea946a0b25943b48c927b78
Priyanka Ankush Jadhav <pj0830939@gmail.com>
2025-12-25 21:14:50 +0530

    Add files via upload

---
+++ Assignments/Assignment No.01/14 Program of Calculate  square .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdio.h>
+
+int main()
+{
+  float Num = 0 ,squr = 0;
+  
+  printf("\n Enter a number:");
+  scanf("%f",&Num);
+  
+  squr = Num * Num;
+  
+  printf("\n square of the number=%f", squr);
+  
+  getch();
+  return 0;
+}
