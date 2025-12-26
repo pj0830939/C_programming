22b2212e2d98005ca21e2618cdffb83fb8ae3c43
Priyanka Ankush Jadhav <pj0830939@gmail.com>
2025-12-26 23:06:32 +0530

    Add files via upload

---
+++ Assignments/Assignment No.02/4 Program of  Divisible .c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdio.h>
+
+int main()
+{
+   int num;
+   clrscr();
+   
+   printf("Enter the number:");
+   scanf("%d",&num);
+   
+   if(7==0)
+   {
+       printf("%d is divisible 7");
+   }
+   else
+   { 
+       printf("%d is not divisible 7");
+   }
+   
+  getch();
+  return 0;
+}
