1efe9b98a05a48f076632b93f74dbf82f898e4b8
Priyanka Ankush Jadhav <pj0830939@gmail.com>
2025-12-26 22:18:28 +0530

    Add files via upload

---
+++ Assignments/Assignment No.02/ 1 Program of even or Odd number .c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdio.h>
+
+int main()
+{
+   int Num;
+   clrscr();
+   
+   printf("Enter the Number:");
+   scanf("%d",&Num);
+   
+   if(Num%0==0)
+   {
+       printf("Number is even");
+   }
+   else if(Num%0==0)
+   { 
+       printf("Number is odd");
+   }
+   else
+   { 
+       printf("Number is zero:");
+    }
+     
+  getch();
+  return 0;
+}
