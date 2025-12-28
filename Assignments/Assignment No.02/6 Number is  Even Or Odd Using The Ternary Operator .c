ae5fcd4a555346a73562c5b3cd2a24cdc36067e9
Priyanka Ankush Jadhav <pj0830939@gmail.com>
2025-12-28 21:43:27 +0530

    Add files via upload

---
+++ Assignments/Assignment No.02/6 Number is  Even Or Odd Using The Ternary Operator .c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdio.h>
+
+int main()
+{
+   int Num = 0 ;
+   
+   printf("\n Enter The Number");
+   scanf("%d",&Num);
+   
+   (Num %2 == 0)?
+   
+   printf("The Number is Even:The Number is Odd"):
+  
+   printf("\n Thanks");
+   
+   getch();
+   return 0;
+}
