6db220078cae861e7c512a4eb9f579c94ca707ac
Priyanka Ankush Jadhav <pj0830939@gmail.com>
2025-12-28 22:19:40 +0530

    Add files via upload

---
+++ Assignments/Assignment No.03/1 Print The Number of 101 to 110 loop .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdio.h>
+
+int main()
+{
+   int Num = 0;
+   
+   for (Num = 101;Num <= 110;Num++)
+   {
+   printf("%d\n",Num);
+   }
+   
+   printf("\n Thanks");
+   
+   getch();
+   return 0;
+}
