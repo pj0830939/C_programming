43f65f773ec0e5fd689f9124ec66ce7a02e151ba
Priyanka Ankush Jadhav <pj0830939@gmail.com>
2025-12-28 22:20:02 +0530

    Add files via upload

---
+++ Assignments/2 Print The Number of 10 to 1 in Descending  Order .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdio.h>
+
+int main()
+{
+   int Num = 0;
+   
+   for (Num = 10;Num >= 1;Num--)
+   {
+   printf("%d\n",Num);
+   }
+   
+   printf("\n Thanks");
+   
+   getch();
+   return 0;
+}
