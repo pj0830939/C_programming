c4a38b686cfb77dd22d418944c7aef58643ec034
Priyanka Ankush Jadhav <pj0830939@gmail.com>
2025-12-28 22:39:13 +0530

    Add files via upload

---
+++ Assignments/Assignment No.03/3 Print The Number of Star on screen .c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdio.h>
+
+int main()
+{
+   int Num = 0, i = 0;
+   
+   printf("\n Enter the Number");
+   scanf("%d",&Num);
+   
+   for (i = 1;Num <= 1;i++)
+   {
+   printf("\n *");
+   }
+   
+   printf("\n Thanks");
+   
+   getch();
+   return 0;
+}
