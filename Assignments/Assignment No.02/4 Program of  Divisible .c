3eb8841496f21f7720fa0ef9ceda09e9281b91cc
Priyanka Ankush Jadhav <pj0830939@gmail.com>
2025-12-28 21:42:05 +0530

    Fix divisible by 7 check and initialize num

--- Assignments/Assignment No.02/4 Program of  Divisible .c
+++ Assignments/Assignment No.02/4 Program of  Divisible .c
@@ -3,19 +3,18 @@
 
 int main()
 {
-   int num;
-   clrscr();
+   int num = 0;
    
    printf("Enter the number:");
    scanf("%d",&num);
    
    if(7==0)
    {
-       printf("%d is divisible 7");
+     printf("%d is divisible 7");
    }
    else
    { 
-       printf("%d is not divisible 7");
+     printf("%d is not divisible 7");
    }
    
   getch();
