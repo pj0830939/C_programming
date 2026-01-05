8b743d0da6971e486245ff23e221a21a9d5f17a5
Priyanka Ankush Jadhav <pj0830939@gmail.com>
2026-01-05 22:44:43 +0530

    Add files via upload

---
+++ Assignments/Assignment No.03/09 write a program to print the letter .c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char ch = '\0';
+  
+    printf("\n Enter a letter");
+    scanf("\n",&ch);
+  
+    printf("\n OUTPUT => \n\n");
+  
+    if( ch >= 'A' && ch <= 'Z')
+   {
+       while(ch <= 'Z')
+      {
+        printf(" %c ",ch);
+        ch++;
+      }
+   }
+   else if(ch >= 'a' && ch <= 'z')
+   {
+       while(ch >= 'a')
+     {
+        printf(" %c ",ch);
+        ch--;
+     }
+   }
+   else
+   {
+     printf("\n Invalid ");
+   }
+   printf("\n\n Thanks ");
+   
+   getch();
+   return 0;
+}
