59b0e0ccd243df41545f8fa339121995fbb7f516
Priyanka Ankush Jadhav <pj0830939@gmail.com>
2026-01-09 23:13:38 +0530

    Add files via upload

---
+++ Assignments/Assignment No.07/01 Array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+  char Array [5];
+  
+  printf("\n value of 1 Element = <%c>.",Array[0]);
+  printf("\n value of 2 Element = {%c}.",Array[1]);
+  printf("\n value of 3 Element = [%c].",Array[2]);
+  printf("\n value of 4 Element = |%c|.",Array[3]);
+  printf("\n value of 5 Element = *%c*.",Array[4]);
+  
+  printf("\n Thanks.....");
+  
+  getch();
+  return 0;
+}
