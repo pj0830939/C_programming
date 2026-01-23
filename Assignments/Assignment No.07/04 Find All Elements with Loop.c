6bb860ca998b3e013223a822f4f236c9e4ca43fe
Priyanka Ankush Jadhav <pj0830939@gmail.com>
2026-01-23 21:39:24 +0530

    Add files via upload

---
+++ Assignments/Assignment No.07/04 Find All Elements with Loop.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+  int Num [7];
+  
+  printf("\n Array of all elements");
+  
+  printf("\n Enter the 1st element");
+  scanf("%d",&Num);
+  
+  printf("\n Enter the 2nd element");
+  scanf("%d",&Num);
+  
+  printf("\n Enter the 3rd element");
+  scanf("%d",&Num);
+  
+  printf("\n Enter the 4th element");
+  scanf("%d",&Num);
+  
+  printf("\n Enter the 5th element");
+  scanf("%d",&Num);
+  
+  printf("\n Enter the 6th element");
+  scanf("%d",&Num);
+  
+  printf("\n Enter the 7th element");
+  scanf("%d",&Num);
+  
+  printf("\n Enter the 1st element:%d",Num[0]);
+  printf("\n Enter the 2nd element:%d",Num[1]);
+  printf("\n Enter the 3rd element:%d",Num[2]);
+  printf("\n Enter the 4th element:%d",Num[3]);
+  printf("\n Enter the 5th element:%d",Num[4]);
+  printf("\n Enter the 6th element:%d",Num[5]);
+  printf("\n Enter the 7th element:%d",Num[6]);
+  
+  getch();
+  return 0;
+}
