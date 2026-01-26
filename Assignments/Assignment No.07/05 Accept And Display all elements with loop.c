40ea6d1e11bcd430d3e1034d54a6f38066fae993
Priyanka Ankush Jadhav <pj0830939@gmail.com>
2026-01-26 21:51:45 +0530

    Add files via upload

---
+++ Assignments/Assignment No.07/05 Accept And Display all elements with loop.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+  int Array[7] = {0}, i;
+  
+  printf("\n Enter 7 Elements");
+  
+  for(i = 0; i < 7; i++)
+  {
+    scanf("%d",&Array[i]);
+  }
+  printf("Array Element Are:\n");
+  
+  for(i = 0; i < 7; i++)
+  {
+    printf("%d",Array[i]);
+    [i]);
+  }
+  printf("\n Thanks....");
+  
+  getch();
+  return 0;
+}
