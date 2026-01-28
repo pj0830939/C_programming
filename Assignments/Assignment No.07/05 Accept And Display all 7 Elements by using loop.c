756a3f9c8c66b5d0bbfab43c39c917e3883e559d
Priyanka Ankush Jadhav <pj0830939@gmail.com>
2026-01-28 14:23:08 +0530

    Add files via upload

--- Assignments/Assignment No.07/05 Accept And Display all 7 Elements by using loop.c
+++ Assignments/Assignment No.07/05 Accept And Display all 7 Elements by using loop.c
@@ -2,21 +2,21 @@
 #include<conio.h>
 int main()
 {
-  int Num[7] = {}, i = 0;
+  char chr[7] = {}, i = 0;
   
   printf("\n Enter All Elements");
   
   for(i = 1; i <= 7; i++)
   {
     printf("\n Enter Element No %d:",i);
-    scanf("%d",&Num[i]);
+    scanf("%d",&chr[i]);
   }
    printf("\n Press Any Key To See All 7 Value \n");
    getch();
    
   for(i = 1; i <= 7; i++)
   {
-    printf("\n value of %d Element %d",i,Num[i]);
+    printf("\n value of %d Element %d",i,chr[i]);
   }
   getch();
   return 0;
