d03d2c3872bbd0db79956f539700b51937aaec79
Priyanka Ankush Jadhav <pj0830939@gmail.com>
2026-02-23 22:06:56 +0530

    Add files via upload

---
+++ Program of Tables/01 program of tables fixed range_C.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+  int i = 0, j = 0;
+  
+  for(i = 1;i <= 10;i++)
+  {
+    for(j = 1;j <= 13;j++)
+    {
+      printf(" %3d ",i * j);
+    }
+    getch();
+    printf("\n");
+  }
+   printf("\n============******========\n");
+  
+   getch();
+   return 0;
+}
